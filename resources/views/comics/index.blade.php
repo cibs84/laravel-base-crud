d936fc526869c68e23a9d8278bed71de014e7f90
cibs84 <d.tilesi@gmail.com>
2022-08-30 19:57:01 +0200

    show done

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,19 +1,15 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>La nostra lista di fumetti</h1>
+    <h1>I nostri fumetti</h1>
     @foreach ($comics_array as $comic)
         <div>
-            <a href="{{ route('comics.show', [$comic['id']]) }}">
-                <img src="{{ $comic['thumb'] }}" alt="" width="200px">
+            <a href="{{ route('comics.show', [$comic->id]) }}">
                 <div>
-                    <div>Titolo: {{ $comic['title'] }}</div>
-                    <div>Descrizione: {{ $comic['description'] }}</div>
-                    <div>Copertina: {{ $comic['thumb'] }}</div>
-                    <div>Prezzo: {{ $comic['price'] }}</div>
-                    <div>Serie: {{ $comic['series'] }}</div>
-                    <div>Data Sconto: {{ $comic['sale_date'] }}</div>
-                    <div>Tipologia: {{ $comic['type'] }}</div>
+                    <div>Titolo: {{ $comic->title }}</div>
+                    <div>Serie: {{ $comic->series }}</div>
+                    <div>Tipologia: {{ $comic->type }}</div>
+                    <div>Prezzo: {{ $comic->price }}</div>
                 </div>
             </a>
         </div>
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>{{ $comic['title'] }}</h1>
+    <div>
+        <img src="{{ $comic->thumb }}" alt="" width="200px">
+        <div>
+            <div>Titolo: {{ $comic->title }}</div>
+            <div>Descrizione: {{ $comic->description }}</div>
+            <div>Copertina: {{ $comic->thumb }}</div>
+            <div>Prezzo: {{ $comic->price }}</div>
+            <div>Serie: {{ $comic->series }}</div>
+            <div>Data Sconto: {{ $comic->sale_date }}</div>
+            <div>Tipologia: {{ $comic->type }}</div>
+        </div>
+    </div>
+@endsection
